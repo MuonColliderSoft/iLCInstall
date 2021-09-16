aee417fceb83b6f1892bdd492dca8e358a520c5c
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2021-09-16 16:30:02 +0200

    Fix for variable MARLIN_DLL

--- ilcsoft/marlin.py
+++ ilcsoft/marlin.py
@@ -80,3 +80,56 @@ class Marlin(BaseILC):
                 if( qt != None and Version( qt.version ) < '4.0' ):
                     self.abort( "you need QT 4!! QT version " + qt.version + " found..." )
 
+    def writeEnv(self, f, checked):
+        """ helper function used for writing the environment to a file """
+        
+        # resolve circular dependencies
+        if( self.name in checked ):
+            return
+        else:
+            checked.append( self.name )
+
+        if self.env or sum(map(len, self.envpath.values()), 0):
+            f.write( 2*os.linesep + "#" + 80*'-' + os.linesep + "#" + 5*' ' \
+                    + self.name + os.linesep + "#" + 80*'-' + os.linesep )
+        
+        # first write the priority values
+        for k in self.envorder:
+            f.write( "export " + str(k) + "=\"" + str(self.env[k]) + "\"" + os.linesep )
+        # then write the rest
+        for k, v in self.env.iteritems():
+            if k not in self.envorder:
+                f.write( "export " + str(k) + "=\"" + str(self.env[k]) + "\"" + os.linesep )
+    
+        f.write( "# --- additional " + self.name + " commands ------- " + os.linesep ) 
+        for c in self.envcmds:
+            f.write( c + os.linesep ) 
+
+        # list of "trivial" paths we do not want to add again to PATH and co
+        ignorepaths = ['/usr/bin','/usr/lib','/sbin','/usr/sbin']
+        # path environment variables
+        for k, v in self.envpath.iteritems():
+            if( len(v) != 0 ):
+                # expand every variable we introduced previously
+                exp = str().join(v)
+                for e, ev in self.env.iteritems():
+                    p = re.compile(r"\$"+str(e)) # compile regular expression to match shell variable
+                    exp = p.sub(str(ev), exp)  # replace with expanded variable for absolute path
+                # check for match
+                if exp in ignorepaths:
+                    continue
+                path = str.join(':', v)
+                if k == "MARLIN_DLL":
+                    f.write( "export " + k + "=\"" + path + "\"" + os.linesep )
+                else:
+                    f.write( "export " + k + "=\"" + path + ":$" + k + "\"" + os.linesep )
+
+        if( len(checked) > 1 ):
+            mods = self.optmodules + self.reqmodules
+        else:
+            # buildonly modules are only written for the package were they are needed
+            mods = self.optmodules + self.reqmodules + self.reqmodules_buildonly + self.reqmodules_external
+        
+        for modname in mods:
+            self.parent.module(modname).writeEnv(f, checked)
+
